1edf681a73353e8572d6164e77d897de81acd5b2
Adam Dyson <contact@adamdyson.com.au>
2017-09-27 17:32:48 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Verify the installed location
+# Verify the installed location.
 docker exec --tty ${container_id} env TERM=xterm which cpdf
-# Verify the installed version
+
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm cpdf -version
